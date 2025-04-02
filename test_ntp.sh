4ac2579710eb522ba36cb296a6ac5029001e3ec7
ItsLance <89491543+ItsLance@users.noreply.github.com>
2025-04-02 17:36:01 -0400

    Update test_ntp.sh
    modified output

--- test_ntp.sh
+++ test_ntp.sh
@@ -11,22 +11,27 @@ TARGET="$1"
 echo "==== Basic NTP Server Check for $TARGET ===="
 
 echo "== Installing necessary packages...==="
-sudo apt install ntp sntp python pip iputils-ping ntpdate nano netcat-traditional netcat-openbsd nmap tcdump
+sudo apt install ntp sntp python3 pip iputils-ping ntpdate nano netcat-traditional netcat-openbsd nmap tcpdump
+sleep 3
 
 # Check if NTP port is open using nmap (more reliable than nc)
 echo "[1] Checking if NTP port is open..."
 nmap -p 123 -sU $TARGET
+sleep 3
 
 # Try a simple ntpdate query
 echo "[2] Testing with ntpdate (simple query)..."
 ntpdate -q $TARGET
+sleep 3
 
 # Try a more detailed ntpdate query
 echo "[3] Testing with ntpdate (detailed query)..."
 ntpdate -d $TARGET
+sleep 3
 
 # Try ntpq peek command
 echo "[3] Testing with ntpq...(likely to time out due to ntp.conf but still worth a shot)"
 echo "rv" | ntpq -p $TARGET
+sleep 5
 
 echo "==== NTP Server Check completed ===="
